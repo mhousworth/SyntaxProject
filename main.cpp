734e4f4c6c0e16fb91a38380f7fa8411e99d7a41
migz333 <mgutierrez95@go.pasadena.edu>
2020-05-15 02:27:46 -0700

    printing out identifer not declared

--- main.cpp
+++ main.cpp
@@ -224,7 +224,7 @@ int main(){
                     }
                     else{
                         // Error: Identifier was not declared
-                        std::cout << "Identifier was not declared\n";
+                        std::cout << "Identifier: " << lexemeToken[index].first << " was not declared\n";
                     }
                 }
 
